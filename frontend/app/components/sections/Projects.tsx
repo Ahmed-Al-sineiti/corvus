72f0e5885d4d2906738bcdd6b8c4194f067dd37e
jabr <jabr@gmail.com>
2026-09-01 00:43:43 +0300

    new edits

--- frontend/app/components/sections/Projects.tsx
+++ frontend/app/components/sections/Projects.tsx
@@ -8,7 +8,7 @@ const VIDEOS = [
   "https://res.cloudinary.com/oilun9qx/video/upload/v1787795664/takya.mp4",
   "https://res.cloudinary.com/oilun9qx/video/upload/v1787795693/kingsfield.mp4",
   "https://res.cloudinary.com/spkrgysz/video/upload/v1787871925/Tabarak_Compressed.mp4",
-  "https://res.cloudinary.com/oilun9qx/video/upload/v1787795693/kingsfield.mp4",
+  "https://player.mux.com/GEmzKnV02qrg7ADfxOi00KiwBZOXfFNN4dwTCqR7aQj02Q",
 ];
 
 export default function Projects() {
